88bddcdecf4fbdc94703715d5af8664e5e94cd2e
Dragos Radu <dragosthealx@gmail.com>
2015-08-22 18:36:21 +0300

    merge

---
+++ app/LudumDare33/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0f, 
+		                                  Input.GetAxis ("Vertical") * speed * Time.deltaTime));
+
+	}
+}
