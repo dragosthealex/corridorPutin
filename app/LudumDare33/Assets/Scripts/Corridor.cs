eda9c4f720481e11edd830a665625141cdd2a587
Dragos Radu <dragosthealx@gmail.com>
2015-08-22 12:23:52 +0300

    Started implementing turns and shit with prefabs

--- app/LudumDare33/Assets/Scripts/Corridor.cs
+++ app/LudumDare33/Assets/Scripts/Corridor.cs
@@ -6,7 +6,11 @@ public class Corridor : MonoBehaviour {
 	// Size of the grid
 	public IntVector2 size;
 	// A single cell
-	public CorridorCell corridorCellPrefab;
+	public CorridorCell corridorCellInfundN;
+	public CorridorCell corridorCellCornerN_W;
+	public CorridorCell corridorCellCorridorN_S;
+	public CorridorCell corridorCellWallToE;
+	
 	// Delay for generation (debug)
 	public float generationStepDelay;
 	// Probability for a corner
@@ -34,34 +38,57 @@ public class Corridor : MonoBehaviour {
 
 	// Gets the cell at given coords
 	public CorridorCell GetCell(IntVector2 coordinates) {
-		return cells [coordinates.x, coordinates.z];
+		if (ContainsCoordinates (coordinates)) {
+			return cells [coordinates.x, coordinates.z];
+		} else {
+			return null;
+		}
 	}// getcell
 	
 	// Generate the corridor
 	public void Generate() {
 		cells = new CorridorCell[size.x, size.z];
+		bool turned = false, turnedRecently = false;
 		IntVector2 coordinates = RandomCoordinates;
 		CorridorDirection nextDirectionName = CorridorDirections.RandomValue;
 		CorridorDirection lastDirectionName = nextDirectionName;
 		IntVector2 nextDirection = nextDirectionName.ToIntVector2();
 
 		while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null) {
-			CreateCell(coordinates);
-			if(Random.Range(0, 100) < cornerProbability) {
+			CreateCell(coordinates, lastDirectionName, nextDirectionName);
+			turned = false;
+			if(Random.Range(0, 100) < cornerProbability && !turnedRecently) {
 				nextDirectionName = CorridorDirections.RandomValue;
 				while(nextDirectionName == CorridorDirections.OppositeOf(lastDirectionName)) {
 					nextDirectionName = CorridorDirections.RandomValue;
 				}
 				lastDirectionName = nextDirectionName;
 				nextDirection = nextDirectionName.ToIntVector2();
+				turned = true;
 			}// if
+			turnedRecently = turned;
 			coordinates += nextDirection;
 		}// while
 	}// generate
 
 	// Create a single corridor cell
-	private void CreateCell (IntVector2 coordinates) {
-		CorridorCell newCell = Instantiate (corridorCellPrefab) as CorridorCell;
+	private void CreateCell (IntVector2 coordinates, CorridorDirection lastDirection, CorridorDirection direction) {
+		// Cell's neighbors
+		CorridorCell north = GetCell (new IntVector2(coordinates.x, coordinates.z + 1));
+		CorridorCell south = GetCell (new IntVector2(coordinates.x, coordinates.z - 1));
+		CorridorCell east = GetCell (new IntVector2(coordinates.x + 1, coordinates.z));
+		CorridorCell west = GetCell (new IntVector2(coordinates.x - 1, coordinates.z));
+
+		// The new cell
+		CorridorCell newCell;
+		// If the first cell
+		if (north == null && south == null && east == null && west == null) {
+			newCell = Instantiate (corridorCellInfundN) as CorridorCell;
+			newCell.PointFromTo (lastDirection, direction);
+		} else {
+			newCell = Instantiate (corridorCellCorridorN_S) as CorridorCell;
+			newCell.PointFromTo (lastDirection, direction);
+		}
 		cells [coordinates.x, coordinates.z] = newCell;
 		newCell.material.color = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
 		newCell.name = "Corridor Cell " + coordinates.x + ", " + coordinates.z;
--- app/LudumDare33/Assets/Scripts/CorridorCell.cs
+++ app/LudumDare33/Assets/Scripts/CorridorCell.cs
@@ -7,14 +7,31 @@ public class CorridorCell : MonoBehaviour {
 	public IntVector2 coordinates;
 	// The material
 	public Material material;
+	// The mesh
+	public Mesh mesh;
 
 	// Use this for initialization
 	void Awake () {
-		material = GetComponentInChildren<Renderer>().material;
+		material = GetComponentInChildren<Renderer> ().material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	// Rotate the cell
+	public void PointFromTo(CorridorDirection from, CorridorDirection to) {
+		if (from == to) {
+			Debug.Log(from + " " + to);
+			if (to == CorridorDirection.North)
+				transform.rotation = Quaternion.Euler(0, 0, 0);
+			else if (to == CorridorDirection.South)
+				transform.rotation = Quaternion.Euler(0, 180, 0);
+			else if (to == CorridorDirection.East)
+				transform.rotation = Quaternion.Euler(0, 90, 0);
+			else if (to == CorridorDirection.West)
+				transform.rotation = Quaternion.Euler(0, 270, 0);
+		}
 	}
 }
