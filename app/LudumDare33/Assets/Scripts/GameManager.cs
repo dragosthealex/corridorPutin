1d8453628d28624e0937335b11e0aef2f9a975cc
Copper <alexcopil1@gmail.com>
2015-08-24 14:36:33 +0300

    GOING OUT FOR AIR

--- app/LudumDare33/Assets/Scripts/GameManager.cs
+++ app/LudumDare33/Assets/Scripts/GameManager.cs
@@ -20,6 +20,8 @@ public class GameManager : MonoBehaviour {
 	public GameObject OLcanvas;
 	public GameObject StartRoom;
 	private void Start () {
+
+
 		OLcanvas = GameObject.FindGameObjectWithTag ("canvas");
 		OLcanvas.gameObject.SetActive (false);
 		StartRoom = GameObject.FindGameObjectWithTag ("startroom");
@@ -37,6 +39,7 @@ public class GameManager : MonoBehaviour {
 
 		Destroy (initialCamera.gameObject);
 
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	private void RestartGame () {
