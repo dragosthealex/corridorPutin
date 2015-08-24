3b296d37bc453d664aa99945271401875c89edde
Copper <alexcopil1@gmail.com>
2015-08-24 03:13:43 +0300

    fix and fix

--- app/LudumDare33/Assets/Scripts/ALexCamMovement.cs
+++ app/LudumDare33/Assets/Scripts/ALexCamMovement.cs
@@ -13,13 +13,13 @@ public class ALexCamMovement : MonoBehaviour {
 		camera = FindObjectOfType<Camera>().gameObject;
 	}
 
-	void Update (){
+	void FixedUpdate (){
 
 		transform.Rotate(0f,Input.GetAxis("Mouse X")*sensitivity,0f);
 
 		camera.transform.Rotate(-Input.GetAxis("Mouse Y")*sensitivity,0f,0f);
 
-		transform.Translate(Input.GetAxis("Horizontal")*speed,0f,Input.GetAxis("Vertical")*speed);
+		transform.Translate(-Input.GetAxis("Horizontal")*speed,0f,-Input.GetAxis("Vertical")*speed);
 
 		camera.transform.rotation = Quaternion.Euler(new Vector3(Mathf.Clamp(camera.transform.rotation.eulerAngles.x,25f,70f),camera.transform.rotation.eulerAngles.y,0f));
 	}
