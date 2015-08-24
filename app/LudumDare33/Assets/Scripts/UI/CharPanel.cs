0e6510118ea3538a2cdfb3316a25b0acf70ed017
Saw <adelinsoare@yahoo.com>
2015-08-24 12:24:59 +0300

    fixt C

--- app/LudumDare33/Assets/Scripts/UI/CharPanel.cs
+++ app/LudumDare33/Assets/Scripts/UI/CharPanel.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CharPanel : MonoBehaviour {
-
+	public CanvasGroup canvasGroup;
 	// Use this for initialization
 	void Start () {
 	
