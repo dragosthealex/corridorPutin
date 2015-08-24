fe6afd298657cebbeb9f3ed745df23ffaabcc555
Copper <alexcopil1@gmail.com>
2015-08-24 04:52:16 +0300

    might fuck shit up

--- app/LudumDare33/Assets/Scripts/ALexCamMovement.cs
+++ app/LudumDare33/Assets/Scripts/ALexCamMovement.cs
@@ -8,18 +8,35 @@ public class ALexCamMovement : MonoBehaviour {
 
 	public GameObject camera;
 
+public float fixer = 1f;
+
 	void Awake (){
 		camera = FindObjectOfType<Camera>().gameObject;
 	}
 
 	void FixedUpdate (){
-
+	
 		transform.Rotate(0f,Input.GetAxis("Mouse X")*sensitivity,0f);
 
-		camera.transform.Rotate(-Input.GetAxis("Mouse Y")*sensitivity,0f,0f);
+//		if (camera.transform.rotation.eulerAngles.x < 30 || camera.transform.rotation.eulerAngles.x > 65)
+//			fixer = 0.01f;
+//		else if (camera.transform.rotation.eulerAngles.x >= 30 && camera.transform.rotation.eulerAngles.x <= 65)
+//			fixer = 1f;
+
+		camera.transform.Rotate(-Input.GetAxis("Mouse Y")*sensitivity*fixer,0f,0f);
+
+
+
+
 
 		transform.Translate(-Input.GetAxis("Horizontal")*speed,0f,-Input.GetAxis("Vertical")*speed);
 
+//		camera.transform.rotation = Quaternion.Euler(new Vector3(Mathf.Clamp(camera.transform.rotation.eulerAngles.x,25f,70f),camera.transform.rotation.eulerAngles.y,0f));
+//		camera.transform.rotation = Quaternion.Euler(new Vector3(Mathf.Clamp(camera.transform.rotation.eulerAngles.x,25f,70f),175f,0f));
+
 		camera.transform.rotation = Quaternion.Euler(new Vector3(Mathf.Clamp(camera.transform.rotation.eulerAngles.x,25f,70f),camera.transform.rotation.eulerAngles.y,0f));
+
+//		camera.transform.localRotation = Quaternion.Euler(camera.transform.localRotation.x,175f,camera.transform.localRotation.z);
+
 	}
 }
